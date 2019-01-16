d9d5c5ef3c9d231f6ea14425d49840a2ce42dd8f
nnmurphy <46692322+nnmurphy@users.noreply.github.com>
2019-01-16 09:45:00 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*******************************************
+ Helloprinter.java
+ *******************************************
+ Lab 1 Introduciton to the Course for Mac
+ ******************************************
+ Natalie Murphy
+ January 16, 2019
+ CMSC 255 Section 003
+ */
+ public class Hello {
+	public static void printHeading() {
+	System.out.println ("Natalie Murphy");
+	System.out.println ("Lab 1 Introduction to the Course for Mac");
+	System.out.println ("This program is for testing the text editor Sublime.");
+	System.out.println ("Sublime Version 3.1.1");
+	System.out.println ("CMSC 255 Section 003");
+	System.out.println ();
+	}
+	public static void main (String [] args) {
+		printHeading();
+		System.out.println ("Hello");
+	}
+ }
+
+
